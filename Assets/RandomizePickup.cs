78c11199e2c46a4248e0016103790fedb24d397e
Christian Grail <christian.grail@sap.com>
2016-07-22 15:14:02 +0200

    Added random pickup generation

---
+++ Assets/RandomizePickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizePickup : MonoBehaviour {
+
+	public GameObject pickupTemplate;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 12; i++) {
+			Instantiate (pickupTemplate, new Vector3 (getRandomValue(), 1.3f, getRandomValue()), Quaternion.identity);
+		}
+	}
+
+	float getRandomValue () {
+		return (Random.value * 17.5f) - 8.75f;
+	}
+
+}
