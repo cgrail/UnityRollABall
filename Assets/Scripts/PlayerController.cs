66588d059ad960a98f0cb4829d3421c112e06809
Christian Grail <christian.grail@sap.com>
2016-07-22 14:23:29 +0200

    Running version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		updateCount ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count++;
+			updateCount ();
+		}
+	}
+
+	void updateCount() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win";
+			Scene currentScene = SceneManager.GetActiveScene ();
+			SceneManager.UnloadScene (currentScene.buildIndex);
+			SceneManager.LoadScene (currentScene.buildIndex);
+		}
+	}
+
+}
